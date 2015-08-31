ca69abc18099c59858846645f9f95a8c74e10feb
gn00466269 <gn00466269@gmail.com>
2015-09-01 00:32:03 +0800

    update FindPwDAOjdbc
    update select by username and select all method

--- EEIT80Team01/src/member/model/dao/FindPwDAOjdbc.java
+++ EEIT80Team01/src/member/model/dao/FindPwDAOjdbc.java
@@ -2,6 +2,12 @@ package member.model.dao;
 
 import global.GlobalService;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.naming.Context;
@@ -11,6 +17,7 @@ import javax.sql.DataSource;
 
 import member.model.FindPwBean;
 import member.model.FindPwDAO;
+import member.model.MemberBean;
 
 public class FindPwDAOjdbc implements FindPwDAO {
 	
@@ -27,21 +34,66 @@ public class FindPwDAOjdbc implements FindPwDAO {
 		}		
     }
 	
+	private static final String SELECT_BY_USERNAME =
+			"select * from findpw where username=?";
 	@Override
 	public FindPwBean select(String userName) {
-
-		return null;
+		FindPwBean bean = null;
+		ResultSet rset = null;
+		
+		try (Connection conn = ds.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(SELECT_BY_USERNAME);){
+				stmt.setString(1, userName);
+				rset = stmt.executeQuery();
+				if(rset.next()) {
+					bean = new FindPwBean();
+					bean.setUserName(rset.getString("username"));
+					bean.setRequestTime(new Date(rset.getDate("REQUEST_TIME").getTime()));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				if (rset!=null) {
+					try {
+						rset.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}		
+		return bean;
 	}
-
+	
+	private static final String SELECT_ALL = "select * from findpw";
 	@Override
 	public List<FindPwBean> select() {
-
-		return null;
+		List<FindPwBean> beans = null;
+		
+		try(Connection conn = ds.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(SELECT_ALL);
+				ResultSet rset = stmt.executeQuery();){
+				beans = new ArrayList<FindPwBean>();
+				
+				while(rset.next()){
+					FindPwBean bean = new FindPwBean();
+					bean.setUserName(rset.getString("username"));
+					bean.setRequestTime(new Date(rset.getDate("REQUEST_TIME").getTime()));
+					beans.add(bean);				
+				}
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		return beans;
 	}
+	
+	
 
 	@Override
 	public FindPwBean insert(FindPwBean bean) {
-
+		
+		
 		return null;
 	}
 
