6cb66ae82935e0e9eb5fe9472d95165d56e45473
gn00466269 <gn00466269@gmail.com>
2015-08-31 19:58:01 +0800

    Update GlobalService
    add Md5 for password encode

--- EEIT80Team01/src/global/GlobalService.java
+++ EEIT80Team01/src/global/GlobalService.java
@@ -1,6 +1,29 @@
 package global;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class GlobalService {
 	public static final String JNDI_DB_NAME = "java:comp/env/jdbc/eeit80team01";
 	
+	
+	public static String getMD5Endocing(String message) {
+		final StringBuffer buffer = new StringBuffer();
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(message.getBytes());
+			byte[] digest = md.digest();
+			
+			for (int i = 0; i < digest.length; ++i) {
+				final byte b = digest[i];
+				final int value = (b & 0x7F) + (b < 0 ? 128 : 0);
+				buffer.append(value < 16 ? "0" : "");
+				buffer.append(Integer.toHexString(value));
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return buffer.toString();
+	}
 }
