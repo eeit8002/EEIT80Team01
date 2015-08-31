28471e2bf4d0580a8a1c8a583edc89679f004016
eavictor <eavictor@me.com>
2015-08-31 19:19:51 +0800

    Add AdminBean
    Add AdminBean under admin.model

---
+++ EEIT80Team01/src/admin/model/AdminBean.java
@@ -0,0 +1,27 @@
+package admin.model;
+
+public class AdminBean {
+	private String username;
+	private String passwd;
+	private int permission;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public int getPermission() {
+		return permission;
+	}
+	public void setPermission(int permission) {
+		this.permission = permission;
+	}
+	
+}
