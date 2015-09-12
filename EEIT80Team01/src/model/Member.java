b354e1d522464deb4a90f1d7838341beeb08e783
CHIA-HSU <CHIA-HSU@CHIA-HSU-PC>
2015-09-12 21:49:33 +0800

    搜尋功能

---
+++ EEIT80Team01/src/model/Member.java
@@ -0,0 +1,144 @@
+﻿package search.model;
+
+import java.io.Serializable;
+
+public class Member implements Serializable {
+	private static final long serialVersionUID = 1L;
+//測試==================
+	String username;
+	String passwd;
+	String id;
+	String fname;
+	String lname;
+//	int tel;
+
+
+
+
+public Member(String username,String  passwd,String id,String fname,String lname) {
+	super();
+	this.username = username;
+//	this.tel=tel;
+	this.passwd=passwd;
+	this.id=id;
+	this.fname=fname;
+	this.lname=lname;
+	
+}
+	public String getPasswd() {
+	return passwd;
+}
+public void setPasswd(String passwd) {
+	this.passwd = passwd;
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getFname() {
+	return fname;
+}
+public void setFname(String fname) {
+	this.fname = fname;
+}
+public String getLname() {
+	return lname;
+}
+public void setLname(String lname) {
+	this.lname = lname;
+}
+	public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+//	public String getAccount() {
+//	return account;
+//}
+//public void setAccount(String account) {
+//	this.account = account;
+//}
+//public int getTel() {
+//		return tel;
+//	}
+//	public void setTel(int tel) {
+//		this.tel = tel;
+//	}
+//=================
+	
+	
+	
+//		String userId;
+//	String password;
+//	String name;
+//	String email;
+//	String tel; 
+//	int experience;
+//	
+//	public Member(String userId, String password, String name, String mail,
+//			String tel, int expericnce) {
+//		super();
+//		this.userId = userId;
+//		this.password = password;
+//		this.name = name;
+//		email = mail;
+//		this.tel = tel;
+//		this.experience = expericnce;
+//	}
+//
+//	public Member() {
+//		super();
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String mail) {
+//		email = mail;
+//	}
+//
+//	public int getExperience() {
+//		return experience;
+//	}
+//
+//	public void setExperience(int expericnce) {
+//		this.experience = expericnce;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//	public String getTel() {
+//		return tel;
+//	}
+//
+//	public void setTel(String tel) {
+//		this.tel = tel;
+//	}
+//
+//	public String getUserId() {
+//		return userId;
+//	}
+//
+//	public void setUserId(String userId) {
+//		this.userId = userId;
+//	}
+
+}
---
+++ EEIT80Team01/src/model/RegisterService.java
@@ -0,0 +1,94 @@
+﻿package search.model;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.*;
+
+import javax.naming.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.*;
+@WebServlet("/search/queryAllMembers.do")//ch04/ex04/queryAllMembers.do
+public class RegisterService extends HttpServlet{
+	private static final long serialVersionUID = 1L;//加的
+	
+	
+	
+	static private List<String> memberIDList = new ArrayList<String>();
+	Context ctx;
+		public RegisterService() throws IOException {
+		
+			super();//加的
+		try {
+			ctx = new InitialContext();
+		} catch (NamingException e) {
+			// TODO 自動產生 catch 區塊
+			e.printStackTrace();
+		}
+	}
+		Connection conn = null;
+		Collection<Member> allMembers = new ArrayList<Member>();
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			String keyword = request.getParameter("keyword");
+			System.out.println(keyword);
+			getAllMembers(keyword);
+			
+			
+			//String keyword="";//因為 RegisterService 第40行 所以才加的
+			RegisterService  rs = new RegisterService();   //呼叫JDBC的Class
+		     	Collection<Member>  coll = rs.getAllMembers(keyword) ;//呼叫JDBC的Class裡的方法  ,keyword加的
+		     	request.setAttribute("AllMembers", coll);//把呼叫JDBC的Class裡的方法變成識別字串
+			RequestDispatcher rd = request.getRequestDispatcher("/search/showAllMembers.jsp");//呼叫showAllMembers.jsp頁面
+			rd.forward(request, response);
+		}//修改
+			
+			
+			//String keyword
+		public Collection<Member> getAllMembers(String keyword)  {
+		try {
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/eeit80team01");//jdbc/eeit80team01  原 MemberDataBase
+			conn = ds.getConnection();
+//			PreparedStatement stmt = conn.prepareStatement("SELECT * from  MEMBER");//原來
+			PreparedStatement stmt = conn.prepareStatement("select * from MEMBER where USERNAME like ?");//改1
+			//String keyword = request.getParameter("keyword");
+			keyword = keyword + "%";
+			stmt.setString(1,keyword );
+			ResultSet rs = stmt.executeQuery();//原來的
+			System.out.println(rs);
+			System.out.println(keyword);
+			Member mem = null;
+//			while (rs.next()) {
+//				mem = new Member(rs.getString(2), rs.getString(3), rs//數字表示sql欄位
+//						.getString(4), rs.getString(5), rs.getString(6), rs
+//						.getInt(7));
+//				allMembers.add(mem);
+//			}
+			
+			while (rs.next()) {
+			mem = new Member(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5));
+			allMembers.add(mem);
+		}
+			
+			return allMembers;
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO 自動產生 catch 區塊
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+		
+}
