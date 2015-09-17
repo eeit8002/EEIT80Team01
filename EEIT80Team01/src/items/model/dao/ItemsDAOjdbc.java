302995ad49bd08e9b2e03efb759299afaa3b46fe
lou <jhih.lou@gmail.com>
2015-09-17 16:35:53 +0800

    fix
    fix

--- EEIT80Team01/src/items/model/dao/ItemsDAOjdbc.java
+++ EEIT80Team01/src/items/model/dao/ItemsDAOjdbc.java
@@ -15,8 +15,6 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import global.GlobalService;
-import items.model.ImagesBean;
-import items.model.ItemPackBean;
 import items.model.ItemsBean;
 import items.model.ItemsDAO;
 
